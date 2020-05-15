5d4ab926149c972e99762e261cd9011658bb359b
Thái Sơn Hoàng <T.S.Hoang@ecs.soton.ac.uk>
2020-05-15 23:38:16 +0100

    Statemachine Containment generator
    Reset the annotation to the current machine and call the UML-B generator for statemachine.

--- ac.soton.xumlb.statemachine/src/ac/soton/xumlb/generator/StatemachineContainmentGenerator.xtend
+++ ac.soton.xumlb.statemachine/src/ac/soton/xumlb/generator/StatemachineContainmentGenerator.xtend
@@ -20,13 +20,16 @@ import ac.soton.eventb.statemachines.Statemachine
 import ac.soton.xeventb.xmachine.IContainmentGenerator
 import org.eclipse.core.runtime.IProgressMonitor
 import org.eclipse.core.runtime.NullProgressMonitor
+import org.eclipse.emf.transaction.RecordingCommand
 import org.eclipse.emf.transaction.TransactionalEditingDomain
 import org.eventb.emf.core.CoreFactory
 import org.eventb.emf.core.machine.Machine
 
 /**
- * @author htson
- *
+ * @author htson - Initial implementation
+ * @version 0.1
+ * @see IContainmentGenerator
+ * @since 0.1
  */
 class StatemachineContainmentGenerator implements IContainmentGenerator {
 	
@@ -36,15 +39,20 @@ class StatemachineContainmentGenerator implements IContainmentGenerator {
 	
 	override generate(Machine mch, Diagram containedComponent, TransactionalEditingDomain editingDomain) {
 		// If the diagrams is an instance of Statemachine then set the translation target annotation.
-		if (containedComponent instanceof Statemachine) {
-			val stm = containedComponent as Statemachine
-			var annotations = stm.annotations
-			val annot = CoreFactory.eINSTANCE.createAnnotation()
-			annot.setSource("ac.soton.diagrams.translationTarget")
-			annot.references.add(mch)
-			annotations.add(annot)
+		val stm = containedComponent as Statemachine
+		val annot = CoreFactory.eINSTANCE.createAnnotation()
+		annot.setSource("ac.soton.diagrams.translationTarget")
+		annot.getReferences().add(mch);
+		val command = new RecordingCommand(editingDomain, "Set Annotations") {
+			override doExecute() {
+				stm.getAnnotations().clear()
+				stm.getAnnotations().add(annot)
+			}
+		};
+		if (command.canExecute()){
+			editingDomain.getCommandStack().execute(command);
 		}
-
+		
 		// Translate the diagram using the translate factory
 		val containmentCommandId = 'ac.soton.eventb.emf.diagrams.generator.translateToEventB'
 		val factory = TranslatorFactory.getFactory()
--- ac.soton.xumlb.statemachine/xtend-gen/ac/soton/xumlb/generator/StatemachineContainmentGenerator.java
+++ ac.soton.xumlb.statemachine/xtend-gen/ac/soton/xumlb/generator/StatemachineContainmentGenerator.java
@@ -19,7 +19,7 @@ import ac.soton.eventb.statemachines.Statemachine;
 import ac.soton.xeventb.xmachine.IContainmentGenerator;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.transaction.RecordingCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.xtext.xbase.lib.Exceptions;
 import org.eventb.emf.core.Annotation;
@@ -27,7 +27,10 @@ import org.eventb.emf.core.CoreFactory;
 import org.eventb.emf.core.machine.Machine;
 
 /**
- * @author htson
+ * @author htson - Initial implementation
+ * @version 0.1
+ * @see IContainmentGenerator
+ * @since 0.1
  */
 @SuppressWarnings("all")
 public class StatemachineContainmentGenerator implements IContainmentGenerator {
@@ -37,13 +40,20 @@ public class StatemachineContainmentGenerator implements IContainmentGenerator {
   @Override
   public void generate(final Machine mch, final Diagram containedComponent, final TransactionalEditingDomain editingDomain) {
     try {
-      if ((containedComponent instanceof Statemachine)) {
-        final Statemachine stm = ((Statemachine) containedComponent);
-        EList<Annotation> annotations = stm.getAnnotations();
-        final Annotation annot = CoreFactory.eINSTANCE.createAnnotation();
-        annot.setSource("ac.soton.diagrams.translationTarget");
-        annot.getReferences().add(mch);
-        annotations.add(annot);
+      final Statemachine stm = ((Statemachine) containedComponent);
+      final Annotation annot = CoreFactory.eINSTANCE.createAnnotation();
+      annot.setSource("ac.soton.diagrams.translationTarget");
+      annot.getReferences().add(mch);
+      final RecordingCommand command = new RecordingCommand(editingDomain, "Set Annotations") {
+        @Override
+        public void doExecute() {
+          stm.getAnnotations().clear();
+          stm.getAnnotations().add(annot);
+        }
+      };
+      boolean _canExecute = command.canExecute();
+      if (_canExecute) {
+        editingDomain.getCommandStack().execute(command);
       }
       final String containmentCommandId = "ac.soton.eventb.emf.diagrams.generator.translateToEventB";
       final TranslatorFactory factory = TranslatorFactory.getFactory();
